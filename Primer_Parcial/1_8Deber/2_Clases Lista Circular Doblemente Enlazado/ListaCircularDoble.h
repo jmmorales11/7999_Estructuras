29cad6b828a78a0bf824df14abd7108857f6c14e
Camilo Andrés Orrico Emme <99036443+caorrico@users.noreply.github.com>
2022-11-22 00:08:23 -0500

    Actualizo

---
+++ Primer_Parcial/1_8Deber/2_Clases Lista Circular Doblemente Enlazado/ListaCircularDoble.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+ * UFA-ESPE
+ * Autores:  Jeimy Marley Morales Sosa & Camilo Andres Orrico Emme
+ * Modificacion: Martes, 18 de noviembre 
+ * Purpose: Listas, realizar el buscar, eliminar, inserta cola, mostrar cola
+ *Nivel: Tercero           NRC:7999
+ ***********************************************************************/
+
+#include "Nodo.h"
+#include <iostream>
+using namespace std;
+class ListaCircularDoble{
+	private:
+		Nodo *primero;
+		Nodo *ultimo;
+		bool listaVacia(){
+			return (this->primero==NULL);
+		}
+	public:
+		ListaCircularDoble();
+		void insertar(int val);
+		void mostrarLista();
+		void buscar(int elementoBuscar);//encontrar primera ocurrencia
+		void eliminar(int elemento);		
+};
