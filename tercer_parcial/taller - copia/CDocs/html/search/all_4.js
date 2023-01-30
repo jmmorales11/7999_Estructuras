0ec812571f8d4d55d752fd298c002b3fc557945c
Camilo Andrés Orrico Emme <99036443+caorrico@users.noreply.github.com>
2023-01-30 00:03:45 -0500

    ACTUALIZO

---
+++ tercer_parcial/taller - copia/CDocs/html/search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['encerartablero_0',['encerarTablero',['../class_alfil.html#a6f5b2bbfad3305f08a1941875bed6478',1,'Alfil::encerarTablero()'],['../class_funcion_a.html#a15a9b3bca4134f84d80eddec52e420f8',1,'FuncionA::encerarTablero()'],['../class_funciones_interfaz.html#a746f4e91fdf4b1037257226ea4955708',1,'FuncionesInterfaz::encerarTablero()'],['../class_tablero.html#aba9c558ae6952ce84f3fa848f54ab420',1,'Tablero::encerarTablero()']]]
+];
