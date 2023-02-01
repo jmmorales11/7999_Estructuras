f5ad49bcf354e9a05767d9bfb4ba611d9030e950
Camilo Andrés Orrico Emme <99036443+caorrico@users.noreply.github.com>
2023-02-01 18:15:34 -0500

    ACTUALIZO

---
+++ tercer_parcial/cubo_magico_1/CuboMagico.cpp
@@ -0,0 +1,53 @@
+/***********************************************************************
+ * Module:  CuboMagico.cpp
+ * Author:  camilo
+ * Modified: miércoles, 1 de febrero de 2023 18:08:37
+ * Purpose: Implementation of the class CuboMagico
+ ***********************************************************************/
+
+#include "CuboMagico.h"
+
+////////////////////////////////////////////////////////////////////////
+// Name:       CuboMagico::getCuadrado()
+// Purpose:    Implementation of CuboMagico::getCuadrado()
+// Return:     int
+////////////////////////////////////////////////////////////////////////
+
+int CuboMagico::getCuadrado(void)
+{
+   return cuadrado;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       CuboMagico::setCuadrado(int newCuadrado)
+// Purpose:    Implementation of CuboMagico::setCuadrado()
+// Parameters:
+// - newCuadrado
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void CuboMagico::setCuadrado(int newCuadrado)
+{
+   cuadrado = newCuadrado;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       CuboMagico::CuboMagico()
+// Purpose:    Implementation of CuboMagico::CuboMagico()
+// Return:     
+////////////////////////////////////////////////////////////////////////
+
+CuboMagico::CuboMagico()
+{
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       CuboMagico::~CuboMagico()
+// Purpose:    Implementation of CuboMagico::~CuboMagico()
+// Return:     
+////////////////////////////////////////////////////////////////////////
+
+CuboMagico::~CuboMagico()
+{
+   // TODO : implement
+}
