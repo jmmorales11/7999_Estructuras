aa57ce89ea36793432ca3e1395df463a4f238b2a
Camilo Andrés Orrico Emme <99036443+caorrico@users.noreply.github.com>
2023-02-22 08:20:09 -0500

    A

--- tercer_parcial/ProyectoTercerParcial1/Aplicativo.cpp
+++ tercer_parcial/ProyectoTercerParcial1/Aplicativo.cpp
@@ -334,7 +334,7 @@ int main(){
 															lis2 = new ListaDoble<string>();
 															op->diferencia(lis, lis1, lis2);
 															cout<<"Conjunto resultante "<<endl;
-												
+															op->mostrar(lis2);
 															system("pause");
 															break;
 														case 2:
@@ -346,7 +346,7 @@ int main(){
 															lis2 = new ListaDoble<string>();
 															op->diferencia(lis1, lis, lis2);
 															cout<<"Conjunto resultante "<<endl;
-												
+															op->mostrar(lis2);
 															system("pause");
 															break;
 														case 0:
