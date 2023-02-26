532405a1ac2e8730284e0fd7abd69e3dd360616b
Camilo Andrés Orrico Emme <99036443+caorrico@users.noreply.github.com>
2023-02-26 13:25:13 -0500

    ACTUALIZO

--- tercer_parcial/ProyectoTercerParcial1/Operaciones.cpp
+++ tercer_parcial/ProyectoTercerParcial1/Operaciones.cpp
@@ -13,186 +13,194 @@ Operaciones<T>::Operaciones(){
 
 template <typename T>
 void Operaciones<T>::mostrar(ListaDoble<T> *lista){
-
-	int x=190;
- 	NodoDoble<T> *aux=lista->getPrimero();
-  	while(aux){
-  		cout<<" ["<<aux->getObjeto()<<"] " ;
+	graficarBigTiempoLineal();
+	int x=190;//O(1)
+ 	NodoDoble<T> *aux=lista->getPrimero();//O(1)
+  	while(aux){//O(n)
+  		cout<<" ["<<aux->getObjeto()<<"] " ;//O(1)
   		letras(aux->getObjeto(),x, 550);
-  		x+=25;
-  		aux=aux->getSiguiente();
+  		x+=25;//O(1)
+  		aux=aux->getSiguiente();//O(1)
   	}
-	cout<<endl;
+	cout<<endl;//O(1)
+	//O(1)+O(1)+O(n)+O(1)+O(1)+O(1)=O(n)
 }
 
 
 template <typename T> 
 void Operaciones<T>::uniones(ListaDoble<T> *lista1, ListaDoble<T> *lista2,ListaDoble<T> *lista3){
-	long inicio = obtenerTiempo();
-    long tiempoEnMicrosegundos;
+	long inicio = obtenerTiempo();//O(1)
+    long tiempoEnMicrosegundos;//O(1)
 	uniones1();
-	NodoDoble<T> *aux=lista1->getPrimero();
-	while(aux!=NULL){
-		lista3->insertarPorCola(aux->getObjeto());
-		aux=aux->getSiguiente();
+	NodoDoble<T> *aux=lista1->getPrimero();//O(1)
+	while(aux!=NULL){//O(n)
+		lista3->insertarPorCola(aux->getObjeto());//O(1)
+		aux=aux->getSiguiente();//O(1)
 	}
-	aux=lista2->getPrimero();
-	while(aux!=NULL){
-		lista3->insertarPorCola(aux->getObjeto());
-		aux=aux->getSiguiente();
+	aux=lista2->getPrimero();//O(1)
+	while(aux!=NULL){//O(n)
+		lista3->insertarPorCola(aux->getObjeto());//O(1)
+		aux=aux->getSiguiente();//O(1)
 	}
-	lista3->repetidos();
+	lista3->repetidos();//O(1)
 	graficarBigTiempoLineal();
 	ejes();
-	long final = obtenerTiempo();
-	tiempo( final - inicio);
+	long final = obtenerTiempo();//O(1)
+	tiempo( final - inicio);//O(1)
+	//O(1)+O(1)+O(1)+O(n)+O(1)+O(1)+O(1)+O(n)+O(1)+O(1)=O(n)
 }
 
 template <typename T> 
 void Operaciones<T>::interseccion(ListaDoble<T> *lista1, ListaDoble<T> *lista2,ListaDoble<T> *lista3){
-	long inicio = obtenerTiempo();
+	long inicio = obtenerTiempo();//O(1)
 	dibujarInterseccion();
 	lista1->repetidos();
 	lista2->repetidos();
-	NodoDoble<T> *aux=lista1->getPrimero();
-	while(aux!=NULL){
-		NodoDoble<T> *aux1=lista2->getPrimero();
-		while(aux1!=NULL){
-			if(aux->getObjeto()==aux1->getObjeto()){
-				lista3->insertarPorCola(aux->getObjeto());
+	NodoDoble<T> *aux=lista1->getPrimero();//O(1)
+	while(aux!=NULL){//O(n)
+		NodoDoble<T> *aux1=lista2->getPrimero();//O(1)
+		while(aux1!=NULL){//O(n)
+			if(aux->getObjeto()==aux1->getObjeto()){//O(n)
+				lista3->insertarPorCola(aux->getObjeto());//O(1)
 			}
-			aux1 = aux1->getSiguiente();
+			aux1 = aux1->getSiguiente();//O(1)
 		}
-		aux = aux->getSiguiente();
+		aux = aux->getSiguiente();//O(1)
 	}
 	ejes();
 	dibujarBigOExponecialAzul();
-	
-	long final = obtenerTiempo();
+	long final = obtenerTiempo();//O(1)
 	tiempo( final - inicio);
+	//O(1)+O(1)+O(n)*O(1)*O(n)*O(1)*O(1)+O(1)=O(n^2)
 }
 
 template <typename T> 
 void Operaciones<T>::complemento(ListaDoble<T> *lista1, ListaDoble<T> *lista2,ListaDoble<T> *lista3){
-	long inicio = obtenerTiempo();
-	NodoDoble<T> *aux=lista1->getPrimero();
-	NodoDoble<T> *aux1=lista3->getPrimero();
+	long inicio = obtenerTiempo();//O(1)
+	NodoDoble<T> *aux=lista1->getPrimero();//O(1)
+	NodoDoble<T> *aux1=lista3->getPrimero();//O(1)
 	if (comparar(lista3,lista1)==true || comparar(lista3,lista2)==true){
 		cout<<"El conjunto A o B no estan dentro del conjutno universal"<<endl;
-	}else{
+	}else{//O(nlog(n))
 		cout<<"Conjunto "<<endl;
-		while(aux1!=NULL){
-			aux=lista1->getPrimero();
-			while (aux!=NULL){
-				if(aux->getObjeto()==aux1->getObjeto()){	
-					lista3->eliminarPorValor(aux1->getObjeto());
+		while(aux1!=NULL){//O(n)
+			aux=lista1->getPrimero();//O(1)
+			while (aux!=NULL){//O(n)
+				if(aux->getObjeto()==aux1->getObjeto()){//O(n)	
+					lista3->eliminarPorValor(aux1->getObjeto());//O(1)
 				}
-				aux=aux->getSiguiente();
+				aux=aux->getSiguiente();//O(1)
 			}
-			aux1=aux1->getSiguiente();
+			aux1=aux1->getSiguiente();//O(1)
 		}	
 	}
 	dibujarBigOExponecialAzul();
 	ejes();
-	long final = obtenerTiempo();
+	long final = obtenerTiempo();//O(1)
 	tiempo( final - inicio);
-	
+	//O(1)+O(1)+O(1)+O(nlog(n))+O(n)*(O(1)*O(n)*(O(1)*O(1))*O(1))=O(n^2)
 }
 
 template <typename T> 
 bool Operaciones<T>::comparar(ListaDoble<T> *lista1, ListaDoble<T> *lista2){
-	long inicio = obtenerTiempo();
-	NodoDoble<T> *aux;
-	NodoDoble<T> *aux1=lista2->getPrimero();
-	bool val=false;
-	int cont;
-	while(aux1!=NULL){
-		cont=0;
-		aux=lista1->getPrimero();
-		while (aux!=NULL){
-			if(aux->getObjeto()==aux1->getObjeto()){
-				cont+=1;	
+	long inicio = obtenerTiempo();//O(1)
+	NodoDoble<T> *aux;//O(1)
+	NodoDoble<T> *aux1=lista2->getPrimero();//O(1)
+	bool val=false;//O(1)
+	int cont;//O(1)
+	while(aux1!=NULL){//O(n)
+		cont=0;//O(1)
+		aux=lista1->getPrimero();//O(1)
+		while (aux!=NULL){//O(n)
+			if(aux->getObjeto()==aux1->getObjeto()){//O(n)
+				cont+=1;//O(1)
 				break;
 			}
-			aux=aux->getSiguiente();
+			aux=aux->getSiguiente();//O(1)
 		}
-		if(cont==0){
-			val=true;
+		if(cont==0){//O(n)
+			val=true;//O(1)
 			break;
 		}
-		aux1=aux1->getSiguiente();
+		aux1=aux1->getSiguiente();//O(1)
 	}
 	ejes();
 	dibujarBigOExponecialAzul();
-	long final = obtenerTiempo();
+	long final = obtenerTiempo();//O(1)
 	tiempo( final - inicio);
+	//O(1)+O(1)+O(1)+O(1)+O(1)+O(n)*(O(1)+O(1))*O(n)*O(1)+O(1)*O(1))+O(1)=O(n^2)
 	return val;
 }
 template <typename T> 
 void Operaciones<T>::diferencia(ListaDoble<T> *lista1, ListaDoble<T> *lista2, ListaDoble<T> *lista3){
-	long inicio = obtenerTiempo();
+	long inicio = obtenerTiempo();//O(1)
 	lista1->repetidos();
 	lista2->repetidos();
 	copiar(lista1,lista3);
-	NodoDoble<T> *aux2;
+	NodoDoble<T> *aux2;//O(1)
 	
-	NodoDoble<T> *aux=lista3->getPrimero();
-	while(aux!=NULL){
-		aux2=lista2->getPrimero();
-		while(aux2!=NULL){
-			if(aux->getObjeto()==aux2->getObjeto()){
-				lista3->eliminarPorValor(aux->getObjeto());
+	NodoDoble<T> *aux=lista3->getPrimero();//O(1)
+	while(aux!=NULL){//O(n)
+		aux2=lista2->getPrimero();//O(1)
+		while(aux2!=NULL){//O(n)
+			if(aux->getObjeto()==aux2->getObjeto()){//O(n)
+				lista3->eliminarPorValor(aux->getObjeto());//O(1)
 			}
-			aux2 = aux2->getSiguiente();
+			aux2 = aux2->getSiguiente();//O(1)
 		}
-		aux = aux->getSiguiente();
+		aux = aux->getSiguiente();//O(1)
 	}
-	long final = obtenerTiempo();
+	long final = obtenerTiempo();//O(1)
 	tiempo( final - inicio);
+	ejes();
+	dibujarBigOExponecialAzul();
+	//O(1)+O(1)+O(1)+O(n)*O(1)*O(n)*O(1)*O(1))+O(1)=O(n^2)
 }
 
 template <typename T> 
 void Operaciones<T>::diferenciaSimetrica(ListaDoble<T> *lista1, ListaDoble<T> *lista2,ListaDoble<T> *lista3){
-	long inicio = obtenerTiempo();	
+	long inicio = obtenerTiempo();//O(1)
 	lista1->repetidos();
 	lista2->repetidos();
-	ListaDoble<T> *obj= new ListaDoble<T>();
+	ListaDoble<T> *obj= new ListaDoble<T>();//O(1)
 	interseccion(lista1,lista2,obj);
 	uniones(lista1,lista2,lista3);
 	cleardevice();
 	diferenciaSimetrica1();
-	NodoDoble<T> *aux=obj->getPrimero();
-	NodoDoble<T> *aux1=lista3->getPrimero();
-	while(aux!=NULL){
-		aux1=lista3->getPrimero();
-		while(aux1!=NULL){
-			if(aux->getObjeto()==aux1->getObjeto()){
-				lista3->eliminarPorValor(aux->getObjeto());
+	NodoDoble<T> *aux=obj->getPrimero();//O(1)
+	NodoDoble<T> *aux1=lista3->getPrimero();//O(1)
+	while(aux!=NULL){//O(n)
+		aux1=lista3->getPrimero();//O(1)
+		while(aux1!=NULL){//O(n)
+			if(aux->getObjeto()==aux1->getObjeto()){//O(n)
+				lista3->eliminarPorValor(aux->getObjeto());//O(1)
 			}
-			aux1=aux1->getSiguiente();
+			aux1=aux1->getSiguiente();//O(1)
 		}
-		aux=aux->getSiguiente();				
+		aux=aux->getSiguiente();//O(1)			
 	}
 	ejes();
 	dibujarBigOExponecialAzul();
-	long final = obtenerTiempo();
+	long final = obtenerTiempo();//O(1)
 	tiempo( final - inicio);
+	//O(1)+O(1)+O(1)+O(1)+O(n)*O(1)*O(n)*O(1)*O(1))+O(1)=O(n^2)
 }
 
 template <typename T>
 ListaDoble<T> Operaciones<T>:: copiar(ListaDoble<T> *lis1,ListaDoble<T> *lis2){
-	NodoDoble<T> *aux = lis1->getPrimero();
-	while(aux!=NULL){
-		lis2->insertarPorCabeza(aux->getObjeto());
-		aux=aux->getSiguiente();
+	NodoDoble<T> *aux = lis1->getPrimero();//O(1)
+	while(aux!=NULL){//O(n)
+		lis2->insertarPorCabeza(aux->getObjeto());//O(1)
+		aux=aux->getSiguiente();//O(1)
 	}
-
+	//O(1)+O(n)+O(1)+O(1)=O(n)
 	graficarBigTiempoLineal();
 	return *lis2;
 }
 template <typename T>
 long Operaciones<T>:: obtenerTiempo(){
-    struct timeval inicio;
-    gettimeofday(&inicio, NULL);
-    return inicio.tv_sec*1000000+inicio.tv_usec;
+    struct timeval inicio;//O(1)
+    gettimeofday(&inicio, NULL);//O(1)
+    return inicio.tv_sec*1000000+inicio.tv_usec;//O(1)
+    //O(1)+O(1)+O(1)=O(1)
 }
